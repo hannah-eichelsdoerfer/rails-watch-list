a4d8fdd2646d2399dfbe498c8d70030592c527d3
Hannah Eichelsdoerfer <hannaheichelsdoerfer@gmail.com>
2021-05-20 16:48:14 +0200

    Lists Routes Controller and Views

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,24 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # index, show, new, create
+  # get root to: 'lists#index'
+  # get '/new', to: 'lists#new' # new needs to be before show
+  # post '/', to: 'lists#create'
+  # get '/:id', to: 'lists#show', as: :list
+  # resources :lists
+  
+  root to: 'lists#index'
+  resources :lists, only: [:index, :new, :create, :show] do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: :destroy
+
+  # root to: 'lists#index'
+  # resources :lists, except: [:edit, :update] do
+  #   resources :bookmarks, only: [:new, :create]
+  #   # resources :reviews, only: :create
+  # end
+  # resources :bookmarks, only: :destroy
+  # # resources :reviews, only: :destroy
+
 end
